9e14b637ea3232307b9c3dae6052d5ba30d7b9c4
leon-notebook <leon.seidel@gmx.de>
2021-12-19 16:05:35 +0100

    Direct YouTube streaming with Pafy added

--- falcon_data.py
+++ falcon_data.py
@@ -1,9 +1,10 @@
-# Arguments --filename (Video filename), --fps (Video frames per second, default: 30), --stage (Stage 1 or stage 2,
-# default: 1), --timedelay' (Negative time (s) between video start and T0, default:0) and --plottype (Plot or scatter
-# data, default: no plot)
+# Arguments --url (Video URL), --start (Start time in video in seconds), --end (End time in video in seconds),
+# --stage (Stage 1 or stage 2, default: 1), --timedelay' (Negative time (s) between video start and T0, default:0)
+# and --plottype (Plot or scatter data, default: no plot)
 
 import re
 import cv2
+import pafy                         # install with: pip install git+https://github.com/Cupcakus/pafy
 import time
 import argparse
 import pytesseract
@@ -18,37 +19,51 @@ start_time = time.time()
 
 def get_falcon_data(arguments):
 
-    video_filename = arguments.filename
-    fps = arguments.fps
+    url = arguments.url
+    video_start_time = arguments.start
+    video_end_time = arguments.end
     stage = arguments.stage
     tf = arguments.timedelay
 
-    df = pd.DataFrame(columns=["t", "v", "h"])
+    video = pafy.new(url)
+    video_title = video.title
+
+    stream_1080mp4 = None
+
+    for stream in video.allstreams:
+        if stream.resolution == "1920x1080" and stream.extension == "mp4":
+            stream_1080mp4 = stream
+
+    if stream_1080mp4 is None:
+        print("No 1080p mp4 stream found")
+        quit()
 
     if stage == '1':
-        csv_filename = video_filename.split('.')[0] + '_stage1.csv'  # Stage 1
+        csv_filename = video_title + '_stage1.csv'  # Stage 1
     elif stage == '2':
-        csv_filename = video_filename.split('.')[0] + '_stage2.csv'  # Stage 2
+        csv_filename = video_title + '_stage2.csv'  # Stage 2
     else:
         quit()
 
-    cap = cv2.VideoCapture(video_filename)
-    number_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_number = 1
+    cap = cv2.VideoCapture(stream_1080mp4.url)
 
-    while cap.isOpened():
+    df = pd.DataFrame(columns=["t", "v", "h"])
 
-        if frame_number == number_of_frames:
-            break
+    milliseconds = 1000
+    frame_number = 0
+    fps = 30
+
+    cap.set(cv2.CAP_PROP_POS_MSEC, video_start_time * milliseconds)
+
+    while True and cap.get(cv2.CAP_PROP_POS_MSEC) <= video_end_time * milliseconds:
 
         frame_number += 1
-
         t_frame = round(tf, 3)
 
         ret, frame = cap.read()
 
         if stage == '1':
-            cropped_frame = frame[960:1005, 103:395]    # Stage 1
+            cropped_frame = frame[960:1005, 103:395]  # Stage 1
         elif stage == '2':
             cropped_frame = frame[960:1005, 1525:1820]  # Stage 2
         else:
@@ -89,19 +104,18 @@ def get_falcon_data(arguments):
 
     df.to_csv(csv_filename, index=False)
 
-    print("Finished! Average time per frame: " + str(round(((time.time() - start_time) / number_of_frames), 3)) + " s.")
+    print("Finished! Average time per frame: " + str(round(((time.time() - start_time) / frame_number), 3)) + " s.")
 
     if args.plottype is not None:
         plot_falcon_data(df, arguments)
 
 
 def plot_falcon_data(df, arguments):
-
-    df = df.dropna()                                            # Delete rows without values
+    df = df.dropna()  # Delete rows without values
 
     # df = df[(np.abs(stats.zscore(df)) < 1.8).all(axis=1)]     # Delete outliers in all columns
-    df = df[(np.abs(stats.zscore(df.v)) < 1.8)]                 # Delete velocity outliers
-    df = df[(np.abs(stats.zscore(df.h)) < 1.8)]                 # Delete altitude outliers
+    df = df[(np.abs(stats.zscore(df.v)) < 1.8)]  # Delete velocity outliers
+    df = df[(np.abs(stats.zscore(df.h)) < 1.8)]  # Delete altitude outliers
 
     if arguments.plottype == "plot":
         plt.plot(df.t, df.v)
@@ -129,19 +143,24 @@ def plot_falcon_data(df, arguments):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read data from SpaceX Falcon 9 starts')
 
-    parser.add_argument('--filename', nargs='?', type=str, help='Video filename')
-    parser.add_argument('--fps', nargs='?', type=float, help='Video frames per second')
+    parser.add_argument('--url', nargs='?', type=str, help='Video URL')
+    parser.add_argument('--start', nargs='?', type=float, help='Start time in video (seconds)')
+    parser.add_argument('--end', nargs='?', type=float, help='End time in video (seconds)')
     parser.add_argument('--stage', nargs='?', choices=['1', '2'], help='Stage 1 or stage 2')
     parser.add_argument('--timedelay', nargs='?', type=float, help='Time (s) between video start and T0')
     parser.add_argument('--plottype', nargs='?', choices=['plot', 'scatter'], help='Plot or scatter data')
 
     args = parser.parse_args()
 
-    if args.filename is None:
-        print("Pleade add a filename with --filename abc.mp4")
+    if args.url is None:
+        print("Pleade add an URL with --url https://youtube.com/abc")
         quit()
-    if args.fps is None:
-        args.fps = 30
+    if args.start is None:
+        print("Pleade add start time in video (seconds) with --start 300")
+        quit()
+    if args.end is None:
+        print("Pleade add end time in video (seconds) with --end 400")
+        quit()
     if args.stage is None:
         args.stage = '1'
     if args.timedelay is None:
