0c0791a3973350eda496b3d3723746600bcbb48c
Leon Seidel <leon.seidel@gmx.de>
2022-01-03 21:03:13 +0100

    Minor bug fixes

--- rocket_data_live.py
+++ rocket_data_live.py
@@ -24,7 +24,7 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 def get_rocket_data(arguments):
     ##################################################################################################################
     # Performance ####################################################################################################
-    every_n = 20                            # Only analyse every nth frame
+    every_n = 15                            # Only analyse every nth frame
     # Plot settings ##################################################################################################
     upper_limit_velo_plot = 30000           # Upper limit of velocity plot
     upper_limit_alti_plot = 250             # Upper limit of altitude plot
